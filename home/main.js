6482538aaac810b05a5cec5059a085f927c2acc9
Mai <Mai2532004PP@gmail.com>
2024-01-07 20:29:31 +0700

    okela

---
+++ home/main.js
@@ -0,0 +1,6 @@
+alert("Ngthqmai xin chào");
+let username;
+username = prompt("Nhập tên đã nhé:");
+console.log(username);
+console.log("Chào mừng " + username + " đến với web của ngthqmai");
+console.log("Tên của mình là AM");
