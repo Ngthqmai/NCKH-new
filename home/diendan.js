55f4155ebcb3636278c9c91470d397853b670324
Mai <Mai2532004PP@gmail.com>
2024-02-29 13:46:56 +0700

    ngày 29/1

--- home/diendan.js
+++ home/diendan.js
@@ -1,11 +1,11 @@
-function toggleLike(postId) {
-    var heartIcon = document.getElementById("heart-icon-" + postId);
-    var likeButton = document.querySelector("#like-button-" + postId);
-    if (heartIcon.classList.contains("clicked")) {
-      heartIcon.classList.remove("clicked");
-      likeButton.classList.remove("clicked");
-    } else {
-      heartIcon.classList.add("clicked");
-      likeButton.classList.add("clicked");
-    }
+function toggleLike() {
+  var heartIcon = document.getElementById("heart-icon");
+  var likeButton = document.querySelector(".like-button");
+  if (heartIcon.classList.contains("clicked")) {
+    heartIcon.classList.remove("clicked");
+    likeButton.classList.remove("clicked");
+  } else {
+    heartIcon.classList.add("clicked");
+    likeButton.classList.add("clicked");
+  }
 }
